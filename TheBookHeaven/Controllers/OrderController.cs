3cd5353e74349b3f07b5fd71d3df97ab351b449f
Ansar <ansarello1990@gmail.com>
2025-05-29 15:40:12 +0800

    Add OrderController for My Orders view [In Progress]

---
+++ TheBookHeaven/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheBookHeaven.Controllers
+{
+    public class OrderController : Controller
+    {
+        // Renders the MyOrder.cshtml view
+        public IActionResult MyOrder()
+        {
+            return View();
+        }
+    }
+}
