da8be120319312f492e3ad0916b751bfdf201b1b
May Thu Kyaing <maythukyaing707@gmail.com>
2025-06-13 13:45:20 +0800

    Migrate done

---
+++ TheBookHeaven/Migrations/20250612145443_SeedBooks.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TheBookHeaven.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Category", "ImageUrl", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Best Sellers", "https://m.media-amazon.com/images/I/71jDBMuqt9L._AC_UF1000,1000_QL80_.jpg", 19.99m, "Little Women" },
+                    { 2, "Fiction", "https://assets.lulu.com/cover_thumbs/8/d/8dzdnj-front-shortedge-384.jpg", 14.99m, "The Great Gatsby" },
+                    { 3, "Non-Fiction", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1703329310i/23692271.jpg", 21.50m, "Sapiens" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ TheBookHeaven/Migrations/20250613014442_ReSeedBooks.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBookHeaven.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReSeedBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://cdn.britannica.com/04/126004-050-EC4DF54F/Dustcover-Louisa-May-Alcott-Little-Women-novel.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://assets.lulu.com/cover_thumbs/8/d/8dzdnj-front-shortedge-384.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1703329310i/23692271.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "/images/book1.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "/images/book2.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "/images/book3.jpeg");
+        }
+    }
+}
