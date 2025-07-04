387e06e4f4655e92763be1f122db62de11826639
Ansar <ansarello1990@gmail.com>
2025-07-04 22:59:03 +0800

    Admin view added (processing)

---
+++ TheBookHeaven/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace TheBookHeaven.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET Admin/Dashboard
+        public IActionResult Dashboard()
+        {
+            // Check if the user is logged in as Admin
+            var role = HttpContext.Session.GetString("Role");
+            if (role != "Admin")
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            ViewBag.Username = HttpContext.Session.GetString("Username");
+            return View();
+        }
+    }
+}
